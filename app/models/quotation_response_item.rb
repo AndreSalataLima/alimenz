f0788ffb16adece731e5cd31e53a8ffde06325be
Andrre Lima <andre.salatalima@gmail.com>
2025-03-21 11:22:54 -0300

    Updated data when creating a quotation. It's now as data_field

--- app/models/quotation_response_item.rb
+++ app/models/quotation_response_item.rb
@@ -2,5 +2,5 @@ class QuotationResponseItem < ApplicationRecord
   belongs_to :quotation_response
   belongs_to :quotation_item
 
-  validates :price, numericality: { greater_than_or_equal_to: 0 }, allow_nil: true
+  validates :price, numericality: { greater_than: 0, message: "deve ser maior que zero" }
 end
