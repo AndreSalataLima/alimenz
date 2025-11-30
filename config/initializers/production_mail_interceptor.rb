896d029b1302b0e8de49d84dfd1e8a22018b7243
AndreSalataLima <andre.salatalima@gmail.com>
2025-11-30 17:47:58 +0100

    Commented production email interceptor

--- config/initializers/production_mail_interceptor.rb
+++ config/initializers/production_mail_interceptor.rb
@@ -1,19 +1,19 @@
-# Redirect outgoing emails to a test inbox while we're validating production notifications.
-if Rails.env.production?
-  class ProductionTestMailInterceptor
-    def self.delivering_email(message)
-      original_to  = Array(message.to)
-      original_cc  = Array(message.cc)
-      original_bcc = Array(message.bcc)
+# # Redirect outgoing emails to a test inbox while we're validating production notifications.
+# if Rails.env.production?
+#   class ProductionTestMailInterceptor
+#     def self.delivering_email(message)
+#       original_to  = Array(message.to)
+#       original_cc  = Array(message.cc)
+#       original_bcc = Array(message.bcc)
 
-      message.to = ["andre.salatalima@gmail.com"]
-      # Preserve original recipients in headers to aid debugging.
-      message.header["X-Original-To"] = original_to.join(", ") if original_to.any?
-      message.header["X-Original-Cc"] = original_cc.join(", ") if original_cc.any?
-      message.header["X-Original-Bcc"] = original_bcc.join(", ") if original_bcc.any?
-      message.cc = message.bcc = nil
-    end
-  end
+#       message.to = ["andre.salatalima@gmail.com"]
+#       # Preserve original recipients in headers to aid debugging.
+#       message.header["X-Original-To"] = original_to.join(", ") if original_to.any?
+#       message.header["X-Original-Cc"] = original_cc.join(", ") if original_cc.any?
+#       message.header["X-Original-Bcc"] = original_bcc.join(", ") if original_bcc.any?
+#       message.cc = message.bcc = nil
+#     end
+#   end
 
-  ActionMailer::Base.register_interceptor(ProductionTestMailInterceptor)
-end
+#   ActionMailer::Base.register_interceptor(ProductionTestMailInterceptor)
+# end
